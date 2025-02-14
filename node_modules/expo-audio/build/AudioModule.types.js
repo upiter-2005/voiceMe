347a1f85876ac438289b2c962f1747ee0b721e9d
upiter-2005 <pavel470245@gmail.com>
2025-02-14 19:35:29 +0000

    remove node_modules

--- node_modules/expo-audio/build/AudioModule.types.js
+++
@@ -1,2 +0,0 @@
-import { SharedObject } from 'expo-modules-core';
-//# sourceMappingURL=AudioModule.types.js.map
--- node_modules/expo-audio/build/utils/options.d.ts
+++
@@ -1,8 +0,0 @@
-import { RecordingOptions } from '../Audio.types';
-export declare function createRecordingOptions(options: RecordingOptions): {
-    extension: string;
-    sampleRate: number;
-    numberOfChannels: number;
-    bitRate: number;
-};
-//# sourceMappingURL=options.d.ts.map
